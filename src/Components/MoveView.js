4d07b812c7f5a0f82d1ab74acc05bbdd505b42e3
HugoWSF <h_brechenmacher@etu-webschoolfactory.fr>
2023-05-19 13:38:11 +0200

    Fight

---
+++ src/Components/MoveView.js
@@ -0,0 +1,131 @@
+import React, { useState, useContext, useEffect } from "react";
+import { MatchContext } from "../Contexts/MatchContext";
+import { useNavigate } from "react-router-dom";
+
+
+const MoveView = ({ currentTurn, setCurrentTurn  }) => {
+	const [moveUser1, setMoveUser1] = useState();
+	const [moveUser2, setMoveUser2] = useState();
+	const [moveWinner, setMoveWinner] = useState();
+	const { currentGame } = useContext(MatchContext);
+	const { postMove, getGames } = useContext(MatchContext);
+	const [text, setText] = useState("");
+
+	async function handleMove(move) {
+		postMove(move, currentTurn)
+			.then(() => getGames())
+			.catch((e) => console.error(e));
+		setCurrentTurn(currentTurn + 1);
+	}
+
+	
+	useEffect(() => {
+		if (
+			currentGame.turns.length > 0 &&
+			currentGame.turns[currentTurn - 2] !== undefined &&
+			currentGame.turns[currentTurn - 2].user2 !== undefined &&
+			currentGame.turns[currentTurn - 2].user1 !== undefined
+		) {
+			switch (currentGame.turns[currentGame.turns.length - 1].user1) {
+				case "rock":
+					setMoveUser1("0");
+					break;
+				case "paper":
+					setMoveUser1("[]");
+					break;
+				case "scissors":
+					setMoveUser1(">8");
+					break;
+				default:
+					setMoveUser1("");
+			}
+
+			switch (currentGame.turns[currentGame.turns.length - 1].user2) {
+				case "rock":
+					setMoveUser2("0");
+					break;
+				case "paper":
+					setMoveUser2("[]");
+					break;
+				case "scissors":
+					setMoveUser2(">8");
+					break;
+				default:
+					setMoveUser2("");
+			}
+
+			switch (currentGame.turns[currentGame.turns.length - 1].winner) {
+				case "user1":
+					setMoveWinner(currentGame.user1.username + " Wins");
+					break;
+				case "user2":
+					setMoveWinner(currentGame.user2.username + " Wins");
+					break;
+				case "draw":
+					setMoveWinner("It's a draw");
+					break;
+				default:
+					setMoveWinner("");
+			}
+		}
+
+		if (currentGame.winner === null) {
+			setText("It's a Draw");
+		  } else if (currentGame.winner && currentGame.winner.username !== undefined) {
+			setText(currentGame.winner.username + " is the winner");
+		  } else {
+			setText("It's a Draw");
+		  }
+	}, [currentTurn, currentGame, currentGame.winner]);
+	const navigate = useNavigate();
+	
+
+	return (
+		<>
+			{currentGame.winner !== undefined ? (
+				<span>
+					<h3>{text}</h3>
+					<button
+						text="Quitter"
+						onClick={() => navigate("/connexion")}
+					/>
+				</span>	
+			) : (
+				<>
+					<div>
+						<span>
+							<img alt="move user1" src={moveUser1} />
+						</span>
+						<h5 >{moveWinner !== undefined ? moveWinner : ""}</h5>
+						<span >
+							<img alt="move user2" src={moveUser2} />
+						</span>
+					</div>
+					<span>
+						<span >
+							<button onClick={() => handleMove("rock")}>
+								0
+							</button>
+							<h6>Pierre</h6>
+						</span>
+						<span>
+							<button onClick={() => handleMove("paper")}>
+								[]
+							</button>
+							<h6>Feuille</h6>
+						</span>
+						<span >
+							<button onClick={() => handleMove("scissors")}>
+								-8
+							</button>
+							<h6>Ciseaux</h6>
+						</span>
+					</span>
+					
+				</>
+			)}
+		</>
+	);
+};
+
+export default MoveView;
--- src/view/FightView.js
+++ src/view/FightView.js
@@ -1,32 +1,50 @@
-/*import React, { useContext, useEffect, useState } from "react";
-import { JeuxContext } from "../contexts/JeuxContext";
-import Jeu from "../components/Jeu";*/
-import Fight from "../Components/Fight";
+import React, { useContext, useEffect, useState } from "react";
+import { MatchContext } from "../Contexts/MatchContext";
+import MoveView from "../Components/MoveView";
 
-const FightView = () => {
-	/*const { getPartie, partie } = useContext(JeuxContext);
-	const [tour, setTour] = useState(1);
 
+const GamesView = () => {
+	const { getGames, currentGame } = useContext(MatchContext);
+	const [currentTurn, setCurrentTurn] = useState(1);
+	
 	useEffect(() => {
-		getPartie();
+		getGames();
+		const intervalId = setInterval(() => {
+			getGames();
+			if (currentGame && currentGame.user2 !== null) {
+				clearInterval(intervalId);
+			}
+			return () => clearInterval(intervalId);
+		}, 5000);
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [tour]);
+	}, [currentTurn]);
 
-	if (partie) {
+	if (currentGame) {
 		return (
 			<div>
-				<Versus />
-				<Jeu tour={tour} setTour={setTour} />
+				<span>
+					<h2>
+						{currentGame.user1.username
+							? currentGame.user1.username
+							: "en attente ..."}
+					</h2>
+					<p>vs</p>
+					<h2 className="text-2xl">
+						{currentGame.user2 !== null
+							? currentGame.user2.username
+							: "en attente ..."}
+					</h2>
+				</span>
+				{currentGame !== undefined ? (
+					<MoveView currentTurn={currentTurn} setCurrentTurn={setCurrentTurn}/>
+				) : (
+					<></>
+				)}
 			</div>
 		);
 	} else {
 		return <></>;
 	}
-    */return(
-    <>
-        Fight
-        <Fight/>
-    </>
-    );
 };
-export default FightView; 
+
+export default GamesView;
